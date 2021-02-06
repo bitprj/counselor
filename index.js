b7bdc2741876dc85c709d5f20c01233f042e40b5
emsesc <emseschen@gmail.com>
2021-02-06 15:37:58 -0500

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ module.exports = (app) => {
    try {
     var yamlfile = await context.octokit.repos.getContent({
       owner: context.payload.repository.owner.login,
-      repo: context.payload.repository,
+      repo: context.payload.repository.name,
       path:".bit/config.yml",
     });
     app.log.info("Attempting to get YAML")
