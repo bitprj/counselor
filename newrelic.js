729a5db3653734729ac61b4ed5feadc1a564dfc7
emsesc <emseschen@gmail.com>
2021-02-18 17:54:20 -0500

    Update newrelic.js

--- newrelic.js
+++ newrelic.js
@@ -13,7 +13,7 @@ exports.config = {
   /**
    * Your New Relic license key.
    */
-  license_key: process.env.NEW_RELIC_LICENSE_KEY,
+  license_key: [''],
   /**
    * This setting controls distributed tracing.
    * Distributed tracing lets you see the path that a request takes through your
