b11d846ff78fb2fec522fcb545b2e41c4684e2e7
Ganning Xu <ganningxu@gmail.com>
2021-07-18 11:31:32 -0400

    remove index.js

--- app.js
+++ app.js
@@ -9,63 +9,63 @@ const steps = require('./functions/steps.js');
 var start;
 
 module.exports = (app) => {
- app.log.info("Yay, the app was loaded!");
- app.on("push", async (context) => {
-  app.log.info("Push event")
+  app.log.info("Yay, the app was loaded!");
+  app.on("push", async (context) => {
+    app.log.info("Push event")
 
-  if (context.payload.commits[context.payload.commits.length - 1].added.includes(".bit/course-details.md")) {
-    console.log("True")
-    start = true
-  } else {
-    console.log("False")
-    start = false
-  }
+    if (context.payload.commits[context.payload.commits.length - 1].added.includes(".bit/course-details.md")) {
+      console.log("True")
+      start = true
+    } else {
+      console.log("False")
+      start = false
+    }
 
-  app.log.info(context.payload.commits[0])
+    app.log.info(context.payload.commits[0])
 
-  if (start && context.payload.commits[0].added[0] != ".bit/.progress") {
-    let configData = await data.yamlFile(context);
-    app.log.info("protecting")
-    console.log("Deleting file...")
-    await steps.deleteFile(context);
-    await steps.startLab(context, configData);
-    console.log("Committing workflow files")
-    await steps.workFlow(context);
-    await steps.protectBranch(context); // can't merge
-  }
- });
+    if (start && context.payload.commits[0].added[0] != ".bit/.progress") {
+      let configData = await data.yamlFile(context);
+      app.log.info("protecting")
+      console.log("Deleting file...")
+      await steps.deleteFile(context);
+      await steps.startLab(context, configData);
+      console.log("Committing workflow files")
+      await steps.workFlow(context);
+      await steps.protectBranch(context); // can't merge
+    }
+  });
 
- app.on('pull_request.ready_for_review', async (context) => {
-   console.log("pull request ready")
-  main(context, "pull_request.ready_for_review");
- })
+  app.on('pull_request.ready_for_review', async (context) => {
+    console.log("pull request ready")
+    main(context, "pull_request.ready_for_review");
+  })
 
- app.on('pull_request.closed', async (context) => {
-  console.log("Pull request")
-  main(context, 'pull_request.closed');
- });
+  app.on('pull_request.closed', async (context) => {
+    console.log("Pull request")
+    main(context, 'pull_request.closed');
+  });
 
- app.on('issue_comment.created', async (context) => {
-  console.log("Issue comment created")
-  if (context.payload.sender.login != "counselorbot[bot]") {
-    main(context, 'issue_comment.created');
-  }
- });
+  app.on('issue_comment.created', async (context) => {
+    console.log("Issue comment created")
+    if (context.payload.sender.login != "counselorbot[bot]") {
+      main(context, 'issue_comment.created');
+    }
+  });
 
- app.on('workflow_run.completed', async (context) => {
-   console.log("Workflow run")
-   console.log(context.payload.workflow_run.name)
-   if (context.payload.workflow_run.name != "Syncing Your Cabin" && context.payload.workflow_run.head_commit.message != "Start workflows" && context.payload.workflow_run.head_commit.message != "Track progress" && context.payload.workflow_run.head_commit.message != "Update progress") {
+  app.on('workflow_run.completed', async (context) => {
+    console.log("Workflow run")
+    console.log(context.payload.workflow_run.name)
+    if (context.payload.workflow_run.name != "Syncing Your Cabin" && context.payload.workflow_run.head_commit.message != "Start workflows" && context.payload.workflow_run.head_commit.message != "Track progress" && context.payload.workflow_run.head_commit.message != "Update progress") {
       main(context, 'workflow_run.completed');
-   }
- });
+    }
+  });
 
- // mark ready for review which triggers check for next step
+  // mark ready for review which triggers check for next step
 
- app.on('create', async (context) => {
-  console.log("Branch created")
-  main(context, 'create')
-});
+  app.on('create', async (context) => {
+    console.log("Branch created")
+    main(context, 'create')
+  });
 };
 
 async function main(context, event) {
@@ -81,7 +81,7 @@ async function main(context, event) {
     console.log("Getting current step!")
     currentStep = await data.findStep(context);
     console.log(currentStep);
-    
+
   } catch (e) {
     console.log(e)
     return;
@@ -110,10 +110,10 @@ async function main(context, event) {
     }
 
     console.log("The type: " + typeOfStep)
-  
+
     let moveOn = await steps.workEvaluation(typeOfStep, context, configData, currentStep);
     console.log("moveOn: " + moveOn)
-  
+
     console.log("Successfully evaluated")
     console.log("Next Step function executing")
     let issueNo = await data.issueNo(context)
@@ -122,7 +122,7 @@ async function main(context, event) {
       return
 
     }
-  
+
     if (moveOn[0] == true) {
       let branchName = await steps.nextStep(currentStep, context, configData, issueNo);
       await steps.updateFiles(typeOfStep, moveOn, currentStep, configyml, branchName, context)
