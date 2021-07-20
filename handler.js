fb75adea355bcd3dc6d6a2bf498c555b8806a304
Ganning Xu <ganningxu@gmail.com>
2021-07-20 16:45:08 -0400

    revert back to heroku

---
+++ handler.js
@@ -0,0 +1,8 @@
+const {
+    createLambdaFunction,
+    createProbot,
+} = require("@probot/adapter-aws-lambda-serverless");
+const appFn = require("./");
+module.exports.webhooks = createLambdaFunction(appFn, {
+    probot: createProbot(),
+});
