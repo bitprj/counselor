e6ff10db3325b5cfa8013fe560fc8cb013ec4a92
Ganning Xu <ganningxu@gmail.com>
2021-08-18 10:04:47 -0400

    testing

--- index.js
+++ index.js
@@ -9,49 +9,49 @@ const steps = require('./functions/steps.js');
 var start;
 
 module.exports = (app) => {
- app.log.info("Yay, the app was loaded!");
- app.on("push", async (context) => {
-  console.log("Push event")
-  if (context.payload.commits[context.payload.commits.length - 1].added.includes(".bit/course-details.md")) {
-    start = true
-  } else {
-    start = false
-  }
+  app.log.info("Yay, the app was loaded!");
+  app.on("push", async (context) => {
+    console.log("Push event")
+    if (context.payload.commits[context.payload.commits.length - 1].added.includes(".bit/course-details.md")) {
+      start = true
+    } else {
+      start = false
+    } // comment lol
 
-  if (start && context.payload.commits[0].added[0] != ".bit/.progress") {
-    let configData = await data.yamlFile(context);
-    console.log("Deleting file...")
-    await steps.deleteFile(context);
-    await steps.startLab(context, configData);
-    console.log("Committing workflow files")
-    await steps.workFlow(context);
-  }
- });
+    if (start && context.payload.commits[0].added[0] != ".bit/.progress") {
+      let configData = await data.yamlFile(context);
+      console.log("Deleting file...")
+      await steps.deleteFile(context);
+      await steps.startLab(context, configData);
+      console.log("Committing workflow files")
+      await steps.workFlow(context);
+    }
+  });
 
- app.on('pull_request.closed', async (context) => {
-  console.log("Pull request")
-  main(context, 'pull_request.closed');
- });
+  app.on('pull_request.closed', async (context) => {
+    console.log("Pull request")
+    main(context, 'pull_request.closed');
+  });
 
- app.on('issue_comment.created', async (context) => {
-  console.log("Issue comment created")
-  if (context.payload.sender.login != "counselorbot[bot]") {
-    main(context, 'issue_comment.created');
-  }
- });
+  app.on('issue_comment.created', async (context) => {
+    console.log("Issue comment created")
+    if (context.payload.sender.login != "counselorbot[bot]") {
+      main(context, 'issue_comment.created');
+    }
+  });
 
- app.on('workflow_run.completed', async (context) => {
-   console.log("Workflow run")
-   console.log(context.payload.workflow_run.name)
-   if (context.payload.workflow_run.name != "Syncing Your Cabin" && context.payload.workflow_run.head_commit.message != "Start workflows" && context.payload.workflow_run.head_commit.message != "Track progress" && context.payload.workflow_run.head_commit.message != "Update progress") {
+  app.on('workflow_run.completed', async (context) => {
+    console.log("Workflow run")
+    console.log(context.payload.workflow_run.name)
+    if (context.payload.workflow_run.name != "Syncing Your Cabin" && context.payload.workflow_run.head_commit.message != "Start workflows" && context.payload.workflow_run.head_commit.message != "Track progress" && context.payload.workflow_run.head_commit.message != "Update progress") {
       main(context, 'workflow_run.completed');
-   }
- });
+    }
+  });
 
- app.on('create', async (context) => {
-  console.log("Branch created")
-  main(context, 'create')
-});
+  app.on('create', async (context) => {
+    console.log("Branch created")
+    main(context, 'create')
+  });
 };
 
 async function main(context, event) {
@@ -85,10 +85,10 @@ async function main(context, event) {
     }
 
     console.log("The type: " + typeOfStep)
-  
+
     let moveOn = await steps.workEvaluation(typeOfStep, context, configData);
     console.log("moveOn: " + moveOn)
-  
+
     console.log("Successfully evaluated")
     console.log("Next Step function executing")
     let issueNo = await data.issueNo(context)
@@ -96,7 +96,7 @@ async function main(context, event) {
     if (issueNo == null) {
       return
     }
-  
+
     if (moveOn[0] == true) {
       let branchName = await steps.nextStep(currentStep, context, configData, issueNo);
       await steps.updateFiles(typeOfStep, moveOn, count, configyml, branchName, context)
